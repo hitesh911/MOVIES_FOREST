9052328da28044b4592a930de143692d8b6f1d84
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2022-01-16 13:42:42 +0300

    new

--- movie_forest/settings.py
+++ movie_forest/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = "django-insecure-*65y(m&u)cp4v@pn1fuwiib2%2oo29_&*djtyzt*a(&+5)vgve
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG  = True
+ALLOWED_HOSTS = ["moviesforest.herokuapp.com" ,  "themoviesforest.herokuapp.com" , "www.themoviesforest.gq", "themoviesforest.gq" , "themoviesforest.com" , "www.themoviesforest.com"]
 
-ALLOWED_HOSTS = ["moviesforest.herokuapp.com" , "localhost" , "themoviesforest-movies-forest.zeet.app" , "www.themoviesforest.gq", "themoviesforest.gq"]
 
 
 # Application definition
